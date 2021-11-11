9d01f9248adbd189ab95fb4c6c045fd80cbe5f52
Dean Lewis <dean.lewis@gateshead.ac.uk>
2021-11-11 14:30:46 +0000

    Implement Interfaces for Services. Closes #5.

---
+++ pet-clinic-data/src/main/java/lewis/dean/petclinic/services/OwnerService.java
@@ -0,0 +1,17 @@
+package lewis.dean.petclinic.services;
+
+import lewis.dean.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+
+}
---
+++ pet-clinic-data/src/main/java/lewis/dean/petclinic/services/PetService.java
@@ -0,0 +1,15 @@
+package lewis.dean.petclinic.services;
+
+import lewis.dean.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+
+}
